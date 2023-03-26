a6e723d7a700e271ee5c5182aac17b3d917fc9d7
Berna <abuyebarnabas@gmail.com>
2023-03-26 22:48:02 +0300

    tmp script file

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+program started.........
+git pull
+
+NEW_PATH=(`pwd`)
+
+for file in $NEW_PATH/*
+do
+	git pull
+	git add $file
+	echo "Input message for $file : "
+	read MSG
+	git commit -m "$MSG"
+	echo "$file - is pushed with a message $MSG\n"
+done
